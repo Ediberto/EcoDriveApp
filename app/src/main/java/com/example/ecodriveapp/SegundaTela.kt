7215d93134b7b162bd18d3d05545a22aede7d00d
Ediberto <edibertosilva33@gmail.com>
2023-08-09 22:13:51 -0300

    Xml da Segunda Tela

--- app/src/main/java/com/example/ecodriveapp/SegundaTela.kt
+++ app/src/main/java/com/example/ecodriveapp/SegundaTela.kt
@@ -1,6 +1,5 @@
 package com.example.ecodriveapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,17 +9,21 @@ class SegundaTela : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda_tela)
-        val voltatelaLogin = findViewById<Button>(R.id.btnVoltarTelaLogin)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val edtprecoAlcool = findViewById<EditText>(R.id.etPrecoAlcool)
         val edtprecoGasolina = findViewById<EditText>(R.id.etPrecoGasolina)
+        val edtprecoOutros = findViewById<EditText>(R.id.etPrecoOutros)
         val txtResultado = findViewById<TextView>(R.id.tvResultado)
         val btnCalcular = findViewById<Button>(R.id.btnCalcular)
         val flexBox = findViewById<CheckBox>(R.id.idFlexCheckBox)
+        val flexBoxGasolina = findViewById<CheckBox>(R.id.idGasolinacheckBox)
+        val flexBoxOutros = findViewById<CheckBox>(R.id.idOutroscheckBox)
         val txtprecoAlcool = findViewById<TextView>(R.id.textprecoAlcool)
         val txtprecoGasolina = findViewById<TextView>(R.id.textprecoGasolina)
-        val flexBoxAlcool = findViewById<CheckBox>(R.id.idAlcoolcheckBox)
-        val flexBoxGasolina = findViewById<CheckBox>(R.id.idGasolinacheckBox)
+        val txtprecoOutros = findViewById<TextView>(R.id.textprecoOutros)
+
         //Definição do OUVINTE PARA O CheckBox
         flexBox.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
@@ -28,24 +31,26 @@ class SegundaTela : AppCompatActivity() {
                 edtprecoAlcool.visibility = View.VISIBLE
                 txtprecoGasolina.visibility = View.VISIBLE
                 edtprecoGasolina.visibility = View.VISIBLE
-                flexBoxAlcool.visibility = View.INVISIBLE
+                flexBoxOutros.visibility = View.INVISIBLE
                 flexBoxGasolina.visibility = View.INVISIBLE
             } else {
-                flexBoxAlcool.visibility = View.VISIBLE
+                flexBoxOutros.visibility = View.VISIBLE
                 flexBoxGasolina.visibility = View.VISIBLE
                 txtprecoAlcool.visibility = View.INVISIBLE
                 edtprecoAlcool.visibility = View.INVISIBLE
                 txtprecoGasolina.visibility = View.INVISIBLE
                 edtprecoGasolina.visibility = View.INVISIBLE
+                txtprecoOutros.visibility = View.INVISIBLE
+                edtprecoOutros.visibility = View.INVISIBLE
             }
         }
-        flexBoxAlcool.setOnCheckedChangeListener { _, isChecked ->
+        flexBoxOutros.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                txtprecoAlcool.visibility = View.VISIBLE
-                edtprecoAlcool.visibility = View.VISIBLE
+                txtprecoOutros.visibility = View.VISIBLE
+                edtprecoOutros.visibility = View.VISIBLE
                 flexBox.visibility = View.INVISIBLE
                 flexBoxGasolina.visibility = View.INVISIBLE
-                txtResultado.text = "Seu veículo só usa Àlcool"
+                txtResultado.text = "Seu veículo utiliza outro tipo de Combustível"
             } else {
                 txtprecoAlcool.visibility = View.INVISIBLE
                 edtprecoAlcool.visibility = View.INVISIBLE
@@ -58,13 +63,17 @@ class SegundaTela : AppCompatActivity() {
             if (isChecked) {
                 txtprecoGasolina.visibility = View.VISIBLE
                 edtprecoGasolina.visibility = View.VISIBLE
-                flexBoxAlcool.visibility = View.INVISIBLE
+                flexBoxOutros.visibility = View.INVISIBLE
+                txtprecoOutros.visibility = View.INVISIBLE
+                edtprecoOutros.visibility = View.INVISIBLE
                 flexBox.visibility = View.INVISIBLE
                 txtResultado.text = "Seu veículo só usa Gasolina"
             } else {
                 txtprecoGasolina.visibility = View.INVISIBLE
                 edtprecoGasolina.visibility = View.INVISIBLE
-                flexBoxAlcool.visibility = View.VISIBLE
+                txtprecoOutros.visibility = View.INVISIBLE
+                edtprecoOutros.visibility = View.INVISIBLE
+                flexBoxOutros.visibility = View.VISIBLE
                 flexBox.visibility = View.VISIBLE
                 txtResultado.text = ""
             }
@@ -81,50 +90,22 @@ class SegundaTela : AppCompatActivity() {
                 txtResultado.setText("É melhor você usar a Gasolina")
             }
         }
+        val btnConsumo: Button = findViewById(R.id.btnCalcular_consumo)
+        val edtLitros: EditText = findViewById(R.id.edttextlitros)
+        val edtDistancia: EditText = findViewById(R.id.edittextdistancia)
+        val tvResultado: TextView = findViewById(R.id.txtMsg)
 
-        voltatelaLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-    }
+        btnConsumo.setOnClickListener {
+
+            val distanciaStr = edtDistancia.text.toString()
+            val litrosStr = edtLitros.text.toString()
+
+            if (distanciaStr.isNotEmpty() && litrosStr.isNotEmpty()) {
+                val distancia: Float = distanciaStr.toFloat()
+                val litros: Float = litrosStr.toFloat()
+                val result: Float = (distancia / litros)
+
+                tvResultado.text = result.toString()
+
+            }}}
 }
-
-/*SEM CHECKBOX
-   override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_segunda_tela)
-        val voltatelaLogin = findViewById<Button>(R.id.btnVoltarTelaLogin)
-
-        val edtprecoAlcool = findViewById<EditText>(R.id.etPrecoAlcool)
-        val edtprecoGasolina = findViewById<EditText>(R.id.etPrecoGasolina)
-        val txtResultado = findViewById<TextView>(R.id.tvResultado)
-        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
-        /*val flexBox = findViewById<CheckBox>(R.id.idFlex)
-        //Definição do OUVINTE PARA O CheckBox
-        if (flexBox.isChecked()) {
-            Toast.makeText(this, "VEICULO É FLEX", Toast.LENGTH_LONG)
-                .show()
-        }*/
-        //AÇÃO DO BOTAO
-        btnCalcular.setOnClickListener{
-            val valorAlcool:Float = edtprecoAlcool.text.toString().toFloat()
-            val valorGasolina:Float = edtprecoGasolina.text.toString().toFloat()
-            val resultadoFinal:Float = valorAlcool/valorGasolina
-            txtResultado.text = "Resultado: $resultadoFinal"
-            if(resultadoFinal <= 0.7) {
-                txtResultado.setText("É melhor você usar o Alcool")
-            } else {
-                txtResultado.setText("É melhor você usar a Gasolina")
-            }
-        /*    if (flexBox.isChecked()) {
-                txtResultado.setText("FLEX")
-                //Toast.makeText(this, "VEICULO É FLEX", Toast.LENGTH_LONG).show()
-            } */
-        }
-
-        voltatelaLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-    }
- */
